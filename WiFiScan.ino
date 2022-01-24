59ab5cf71c2b6acf26d7faa36594f2da087fc768
Assaf David Hillel <68805670+amazingProj@users.noreply.github.com>
2022-01-24 20:39:56 +0200

    Update WiFiScan.ino

--- WiFiScan.ino
+++ WiFiScan.ino
@@ -1,261 +1,163 @@
-/*
-   ....................... Includes section ...................
-*/
-// includes mqtt popular library
+#include <ESP32.h>
 #include <PubSubClient.h>
-#include "WiFi.h"
-#include "WiFiMulti.h"
-#include "stdio.h"
-#include "String.h"
+#include <time.h>
+#include <TZ.h>
+#include <FS.h>
+#include <LittleFS.h>
+#include <CertStoreBearSSL.h>
 
-/***************** defines variables  ***********************/
+// Update these with values suitable for your network.
+const char* ssid = "your_wifi_name";
+const char* password = "your_wifi_password";
+const char* mqtt_server = "712d6a94edd544ddac8b5c44600f18d3.s1.eu.hivemq.cloud";
 
-//WIFI LOGIN information
-#ifndef STASSID
-//CHANGE TO YOUR NETWORK NAME
-#define STASSID "DIR-615-11E2" 
-//CHANGE TO YOUR NETWORK PASSWORD
-#define STAPSK  "77993892" 
-#endif
+// A single, global CertStore which can be used by all connections.
+// Needs to stay live the entire time any of the WiFiClientBearSSLs
+// are present.
+BearSSL::CertStore certStore;
 
-// 0 is false and 1 is true
-#define DEBUG_MODE 0
+WiFiClientSecure espClient;
+PubSubClient * client;
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (500)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
 
-// global variables, change them to change code behavior
-#define LOOPER_DELAY 10000
-#define PORT 3000
-#define ACCESSPOINT_INFO_LENGTH 34
-// how much access point per one message section
-#define ACCESS_POINT_SENT_PER_MESSAGE 3
-
-/*** define symbols ***/
-#define RIGHT_PARENTHESIS "{"
-#define LEFT_PARENTHESIS "}"
-#define COLON_TOKEN ":"
-#define COMMA ","
-
-
-/*************************** global variables *********************/
-//CODE----------------------------
-// set the wifi ssid and password
-const char* ssid     = STASSID;
-const char* password = STAPSK;
-
-
-/// buffers in use in the code 
-char messageBuffer[1024];
-
-//The broker and port are provided by http://www.mqtt−dashboard.com/
-// mqtt variables
-char *mqttServer = "broker.hivemq.com";
-int mqttPort = 1883;
-//Replace these 3 with the strings of your choice
-const char* mqtt_client_name = "ESPYS2111";
-//The topic to which our client will publish
-const char* mqtt_pub_topic = "/ys/testpub"; 
-//The topic to which our client will subscribe
-const char* mqtt_sub_topic = "/ys/testsub"; 
-
-
-
-
-// belongs to the section that make the message in json format
-String mqttMessage = "";
-int FROMStringIndex = 0;
-int TOStringIndex = 0;
-int messageSize = 0;
-int headerOfMqttMessageLength = 0;
-int partialMessageLength = 194;
-int temp = 0;
-int commaNumber = ACCESS_POINT_SENT_PER_MESSAGE;
-
-// for wifi scanning
-WiFiClient client;
-
-// for mqtt messaging
-PubSubClient mqttClient(client);
-
-// for connecting to wifi
-WiFiMulti WiFiMulti;
-
-// for task scheduling
-uint64_t messageTimestamp;
-
-/*********************************** functions ******************************************/
-
-/**
- * calls back some by reference messages from the server
- * @param topic - a pointer to char array
- *        payload - a pointer to bytee array
- *        length - an int represents length of the payload
- */
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message received from: "); Serial.println(topic);
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
   Serial.println();
-  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-}
-
-/**
-   set up lifecycle function of esp32 device
-   the function does:
-   connect the device to wifi access point
-   set up a socket.io connection
-*/
-void setup()
-{
-  Serial.begin(115200);
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  connectWifi();
-  delay(100);
-  mqttClient.setServer(mqttServer, mqttPort);
-  mqttClient.setCallback(callback);
-  Serial.println("Setup done");
-}
+  WiFi.begin(ssid, password);
 
-/**
-   loop lifecycle function do it while the esp32 is connected to power
-   the function does:
-   send the rssi of each access point to server in json format
-*/
-void loop()
-{
-  // gets the time now in miliseconds
-  uint64_t now = millis();
-  if (now - messageTimestamp > LOOPER_DELAY) {
-    messageTimestamp = now;
-    
-    int n = WiFi.scanNetworks();
-    
-    Serial.println("scan done");
-    if (n == 0) {
-      Serial.println("no networks found");
-    } else {
-      Serial.print(n);
-      Serial.println(" networks found");
-      
-      /***** set the initials values of the message ******/
-      mqttMessage = RIGHT_PARENTHESIS;
-      mqttMessage = mqttMessage + "numberOfAccessPoints" + COLON_TOKEN;
-      mqttMessage = mqttMessage + String(n) + COMMA;
-      mqttMessage = mqttMessage + "macAddress" + COLON_TOKEN;
-      mqttMessage = mqttMessage + WiFi.macAddress() + COMMA;
-      mqttMessage = mqttMessage + "Ssid" + COLON_TOKEN;
-      mqttMessage = mqttMessage + WiFi.SSID() + COMMA;
-
-      // save the length of the string (see above ) 
-      headerOfMqttMessageLength = mqttMessage.length();
-      
-      if (DEBUG_MODE) {
-        //Serial.printf("MAC address = %s", WiFi.softAPmacAddress().c_str());
-        Serial.println(WiFi.macAddress());
-      }
-
-      /******************* save all the wifi access point scans in the message string ****************/
-      for (int i = 0; i < n; ++i) {
-        mqttMessage = mqttMessage + String(i + 1) + COLON_TOKEN;
-        
-        mqttMessage = mqttMessage + RIGHT_PARENTHESIS;
-
-        mqttMessage = mqttMessage + "Bssid" + COLON_TOKEN + WiFi.BSSIDstr(i) + COMMA;
-
-        mqttMessage = mqttMessage + "rssi" + COLON_TOKEN + String(WiFi.RSSI(i));
-
-        mqttMessage = mqttMessage + LEFT_PARENTHESIS;
-        // if this is not the last access point put a comma between two differenet access points
-        if (i != n - 1) {
-          mqttMessage = mqttMessage + COMMA;
-        }
-
-      }
-      // make sure that it is connected to mqtt server
-      if (!mqttClient.connected()) {
-        while (!mqttClient.connected()) {
-          if (mqttClient.connect(mqtt_client_name)) {
-            Serial.println("MQTT Connected!");
-            mqttClient.subscribe(mqtt_sub_topic);
-          }
-          else {
-            Serial.print(".");
-          }
-        }
-      }
-
-      
-      mqttMessage = mqttMessage + LEFT_PARENTHESIS;
-      
-      messageSize = mqttMessage.length();
-      partialMessageLength = ACCESS_POINT_SENT_PER_MESSAGE * ACCESSPOINT_INFO_LENGTH;
-      bool firstTime = true;
-
-      Serial.println(mqttMessage);
-      
-      for (FROMStringIndex = 0; FROMStringIndex < messageSize; FROMStringIndex += partialMessageLength) {
-       
-        if (FROMStringIndex + partialMessageLength > messageSize) {
-          TOStringIndex = messageSize;
-        }
-        else {
-          TOStringIndex = FROMStringIndex + partialMessageLength;
-          TOStringIndex += ACCESS_POINT_SENT_PER_MESSAGE * 2;
-        }
-
-        if (firstTime){
-          TOStringIndex += headerOfMqttMessageLength; 
-          
-        }
-        
-        TOStringIndex += commaNumber;
-       
-        mqttMessage.substring(FROMStringIndex, TOStringIndex).toCharArray(messageBuffer, sizeof(messageBuffer));
-        
-        mqttClient.publish(mqtt_pub_topic, messageBuffer);
-     
-        
-        if (firstTime){
-          FROMStringIndex += headerOfMqttMessageLength;
-          
-          firstTime = false;
-        }
-        
-        FROMStringIndex += commaNumber;
-        FROMStringIndex += ACCESS_POINT_SENT_PER_MESSAGE * 2;
-      }
-
-      Serial.println("Message published");
-    }
-    Serial.println("");
-  }
-}
-
-/**
-   connects the device to access point wifi function
-   the function connects the esp32 to wifi.
-   thus it can send requested to server
-*/
-void connectWifi() {
-  //Connects ESP2866 Wifi
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(ssid, password);
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Wait for WiFi... ");
-
-  //Wait until connection is established:
-  while (WiFiMulti.run() != WL_CONNECTED) {
-    Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
   }
-  //Wifi connected, print IP
+
+  randomSeed(micros());
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+
+void setDateTime() {
+  // You can use your own timezone, but the exact time is not used at all.
+  // Only the date is needed for validating the certificates.
+  configTime(TZ_Europe_Berlin, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(100);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println();
+
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.printf("%s %s", tzname[0], asctime(&timeinfo));
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if the first character is present
+  if ((char)payload[0] != NULL) {
+    digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+  }
+}
+
+
+void reconnect() {
+  // Loop until we’re reconnected
+  while (!client->connected()) {
+    Serial.print("Attempting MQTT connection…");
+    String clientId = "ESP8266Client - MyClient";
+    // Attempt to connect
+    // Insert your password
+    if (client->connect(clientId.c_str(), "Esp32", "your_password")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement…
+      client->publish("testTopic", "hello world");
+      // … and resubscribe
+      client->subscribe("testTopic");
+    } else {
+      Serial.print("failed, rc = ");
+      Serial.print(client->state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void setup() {
   delay(500);
+  // When opening the Serial Monitor, select 9600 Baud
+  Serial.begin(9600);
+  delay(500);
+
+  LittleFS.begin();
+  setup_wifi();
+  setDateTime();
+
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
+
+  // you can use the insecure mode, when you want to avoid the certificates
+  //espclient->setInsecure();
+
+  int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+  Serial.printf("Number of CA certs read: %d\n", numCerts);
+  if (numCerts == 0) {
+    Serial.printf("No certs found. Did you run certs-from-mozilla.py and upload the LittleFS directory before running?\n");
+    return; // Can't connect to anything w/o certs!
+  }
+
+  BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
+  // Integrate the cert store with this connection
+  bear->setCertStore(&certStore);
+
+  client = new PubSubClient(*bear);
+
+  client->setServer(mqtt_server, 8883);
+  client->setCallback(callback);
+}
+
+void loop() {
+  if (!client->connected()) {
+    reconnect();
+  }
+  client->loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client->publish("testTopic", msg);
+  }
 }
